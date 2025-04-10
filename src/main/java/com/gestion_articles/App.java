16eec8165302cdd6bfe4e1b6186f90b7f91b0867
corentin GESSE--ENTRESSANGLE <corentin.ckc@gmail.com>
2025-04-10 09:57:46 +0200

    test pipeline on github

--- src/main/java/com/gestion_articles/App.java
+++ src/main/java/com/gestion_articles/App.java
@@ -5,6 +5,6 @@ package com.gestion_articles;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! test");
     }
 }
